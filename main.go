4e75d6945c970ef2a81fb41cf10d5555c6d55ae5
JobaerAhamed <robin_68@live.com>
2023-01-22 20:41:18 +0600

    go math and test

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
-	
+	"strconv"
+
+	"devops/my_math"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -13,6 +17,26 @@ func main() {
 	})
 	e.GET("/shyfur-t", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Shyfur, Sumon")
+	})
+
+	e.GET("/math/sum", func(c echo.Context) error {
+		number1, err := strconv.Atoi(c.QueryParam("number1"))
+
+		if err != nil {
+			log.Println("Error during conversion")
+			return c.String(http.StatusInternalServerError, "Error during number 1 conversion")
+		}
+
+		number2, err2 := strconv.Atoi(c.QueryParam("number2"))
+
+		if err2 != nil {
+			log.Println("Error during conversion")
+			return c.String(http.StatusInternalServerError, "Error during number 1 conversion")
+		}
+
+		return c.JSON(http.StatusOK, echo.Map{
+			"result": my_math.Sum(number1, number2),
+		})
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
---
+++ my_math/math.go
@@ -0,0 +1,5 @@
+package my_math
+
+func Sum(number1 int, number2 int) int {
+	return number1 + number2
+}
